2d0451d77a4b8d4476a617679610b9709f86c720
MakotoKung <angleoftail@gmail.com>
2024-12-15 17:41:41 +0700

    add image

--- main.cpp
+++ main.cpp
@@ -4,33 +4,41 @@
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "Card_game-01");
     
-
-    // run the program as long as the window is open
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Card_game-01");
+    
+    sf::Texture cat_fire; 
+    if(!cat_fire.loadFromFile("assets\\Monster\\Card.png")){
+        std::cout << "Could not load image";
+        return 0 ;
+    }
+    sf::Sprite card; 
+    card.setTexture(cat_fire);
+    
+    sf::Texture Freezing; 
+    if(!Freezing.loadFromFile("assets\\Spell\\Card (1).png")){
+        std::cout << "Could not laod image";
+        return 0;
+    }
+    sf::Sprite Card_Freezing;
+    Card_Freezing.setTexture(Freezing);
+    
+    
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
+            
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         // clear the window with black color
         window.clear(sf::Color::White);
-
-        sf::CircleShape shape(50.f);
-
-        // set the shape color to green
-        shape.setFillColor(sf::Color(100, 250, 50));
-        // window.draw(...);
-        window.draw(shape);
-        
-        // end the current frame
+        //window.draw(card);
+        window.draw(Card_Freezing);
         window.display();
     }
 
